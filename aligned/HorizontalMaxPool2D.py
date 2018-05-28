19a3940ca0bbee60938b063b06800feac4925da0
michuanhaohao <haoluocsc@zju.edu.cn>
2018-05-28 13:56:21 +0800

    add horizontal pooling

---
+++ aligned/HorizontalMaxPool2D.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class HorizontalMaxPool2d(nn.Module):
+    def __init__(self, sz=None):
+        super(HorizontalMaxPool2d, self).__init__()
+
+
+    def forward(self, x):
+        inp_size = x.size()
+        return nn.functional.max_pool2d(input=x,kernel_size= (1, inp_size[3]))
